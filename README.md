9fff790d22bbb43543a55e164eff645486dfd4da
phrayse <80496902+phrayse@users.noreply.github.com>
2024-09-10 14:36:39 +1000

    refactor vis1
    data is read from csv only once, renderChoro() filters all data first, removed antarctica

--- README.md
+++ README.md
@@ -3,13 +3,6 @@ Website Data Visualisation
 
 ## TO DO
 - background image for index, check unsplashed
-- css for all pages -> just use one shared css file don't bother creating three
-    - ~~font stack~~
-    - ~~vars~~
-    - ~~reset~~
-    - 
-- update vis 1 to read entire data and filter for cause
-- crop antarctica from map
-- reduce opacity of irrelevant countries
-- add hover effects for countries
-- 
+- css for all pages
+- html populate select from read-in data
+- positioning of all option elements
--- js/vis1.js
+++ js/vis1.js
@@ -8,111 +8,100 @@ const WIDTH_DEFAULT = "0.5px";
 const WIDTH_HIGHLIGHT = "0.8px";
 
 // Default chart options
+// TODO: remove these => populate category and year dynamically after reading csv is more modular and more interesting to write
 let cause = "Neoplasms";
 let year = 2015;
 let sex = "Total";
 
 // Set up map projection
-var projection = d3.geoMercator()
+let projection = d3.geoMercator()
                     .center([0, 0])
                     .translate([WIDTH / 2, HEIGHT / 2])
                     .scale(150);
 
-var path = d3.geoPath(projection);
+let path = d3.geoPath(projection);
 
 // Set colour range
-var colour = d3.scaleSequential(d3.interpolatePurples); // d3.interpolate gives better colour scaling
+let colour = d3.scaleSequential(d3.interpolatePurples); // d3.interpolate gives better colour scaling
 
 // Set up SVG canvas
-var svg = d3.select("#choropleth")
+let svg = d3.select("#choropleth")
             .append("svg")
             .attr("width", WIDTH)
             .attr("height", HEIGHT);
 
+// Empty array for entire data
+let allCauses = [];
 
-// Read in data from specified file /
+// Read in data from specified file
+d3.csv("../data/causes/all-top-level-causes.csv").then(function(data) {
+    // Convert str to num
+    data.forEach(d => d.Value = +d.Value);
+
+    // Store all data
+    allCauses = data;
+
+    // Render initial view with default options
+    renderChoropleth(cause, year, sex);
+}).catch(error => console.error("Error fetching CSV data: ", error));
+
 // Merge CSV data and GeoJSON map /
 // Render map and apply fill per value
-const renderChoropleth = function(cause, year, sex) {
-    d3.csv(`../data/causes/${cause}.csv`).then(function(data) {
-        // Convert str to num
-        data.forEach(d => d.Value = +d.Value);
+function renderChoropleth(cause, year, sex) {
+    // Have to filter data first to get correct domain for cause
+    let filteredData = allCauses.filter(d => d.Cause === cause && parseInt(d.Year) === year && d.Sex === sex);
 
-        // Set colour domain from new category values
-        colour.domain(d3.extent(data, d => d.Value));
+    // Set colour domain from new category values
+    colour.domain(d3.extent(filteredData, d => d.Value));
 
-        // Create a lookup map to merge CSV and GeoJSON data
-        // If we aren't using Sex in our vis may as well drop /
-        // the column from the data sets entirely, else we /
-        // should add a toggle for M/F/T.
-        var dataMap = new Map();
-        data.filter(d => parseInt(d.Year) === year && d.Sex === sex)
-            .forEach(d => dataMap.set(d.Country, d.Value));
+    // Create a lookup map to merge CSV and GeoJSON data
+    let dataMap = new Map();
+    // split this into two sections -> need to filter before updating domain
+    filteredData.forEach(d => dataMap.set(d.Country, d.Value));
 
+    // Read in GeoJSON file
+    // try figure out if can move this to only read the geojson once but don't think so
+    d3.json("../resources/world.geojson").then(function(json) {
+        // Use lookup map to merge data
+        json.features.forEach(feature => {
+            feature.properties.value = dataMap.get(feature.properties.name) || null;
+        });
 
-        // Read in GeoJSON file
-        d3.json("../resources/world.geojson").then(function(json) {
-            // Use lookup map to merge data
-            json.features.forEach(feature => {
-                feature.properties.value = dataMap.get(feature.properties.name) || null;
-            });
+        // Create path elements
+        svg.selectAll("path")
+            .data(json.features)
+            .join("path")
+            .attr("class", "country")
+            .attr("d", path)
+    
+            // If d.properties.value exists return colour else empty
+            .attr("fill", d => d.properties.value ? colour(d.properties.value) : COLOUR_EMPTY)
+            // .attr("fill", function(d) { // rewrite using ternary ? :
+            //     // Get data value
+            //     let value = d.properties.value;
 
-            // // Merge map and data
-            // for (var i = 0; i < data.length; i++) {
-            //     // Only want one year at a time
-            //     if ((parseInt(data[i].Year) == year) && (data[i].Sex == "Total")) {
-            //         // Get Country and Value
-            //         var country = data[i].Country;
-            //         var dataValue = parseFloat(data[i].Value);
-                    
-            //         // Find corresponding country in GeoJSON
-            //         for (var j = 0; j < json.features.length; j++) {
-            //             var jsonCountry = json.features[j].properties.name;
-
-            //             if (country == jsonCountry) {
-            //                 // Copy the data value into the JSON
-            //                 json.features[j].properties.value = dataValue;
-            //                 break;
-            //             }
-            //         }
+            //     if (value) {
+            //         return colour(value);
+            //     } else {
+            //         return COLOUR_EMPTY;
             //     }
-            // }
+            // })
 
-            // Create path elements
-            svg.selectAll("path")
-                .data(json.features)
-                .join("path") // Replaced enter() & append("path") with join("path")
-                .attr("class", "country")
-                .attr("d", path)
-                .attr("fill", function(d) { // rewrite using ternary ? :
-                    // Get data value
-                    var value = d.properties.value;
+            .attr("stroke", COLOUR_DEFAULT)
+            .attr("stroke-width", WIDTH_DEFAULT)
 
-                    if (value) {
-                        return colour(value);
-                    } else {
-                        return COLOUR_EMPTY;
-                    }
-                })
-                .attr("stroke", COLOUR_DEFAULT)
-                .attr("stroke-width", WIDTH_DEFAULT)
-
-                // mouseover should show pop-up infobox
-                .on("mouseover", function(event, d) {
-                    d3.select(this).style("stroke", COLOUR_HIGHLIGHT);
-                    d3.select(this).style("stroke-width", WIDTH_HIGHLIGHT);
-                })
-                .on("mouseout", function(event, d) {
-                    d3.select(this).style("stroke", COLOUR_DEFAULT);
-                    d3.select(this).style("stroke-width", WIDTH_DEFAULT);
-                });
-        }).catch(error => console.error("Error while loading GeoJSON data:", error));
-    }).catch(error => console.error("Error while loading CSV data:", error));
+            // mouseover should show pop-up infobox
+            // probably move these to separate functions cause will get beefy with the hover stuff
+            .on("mouseover", function(event, d) {
+                d3.select(this).style("stroke", COLOUR_HIGHLIGHT)
+                    .style("stroke-width", WIDTH_HIGHLIGHT);
+            })
+            .on("mouseout", function(event, d) {
+                d3.select(this).style("stroke", COLOUR_DEFAULT)
+                    .style("stroke-width", WIDTH_DEFAULT);
+            });
+    }).catch(error => console.error("Error while loading GeoJSON data:", error));
 };
-
-
-// This section is ugly as gotta fix
-renderChoropleth(cause, year, sex);
 
 // Event listener for category select
 d3.select("#causes").on("change", function() {
@@ -136,8 +125,8 @@ d3.select("#sex").on("change", function() {
 // ~-*-~-*-~
 // TO DO:
 // Clean the event listeners up
-// Handle the calls to renderChoropleth() nicer
 // Move mouseover events to separate functions
 // Add mouseover pop-ups
-// Path fill function probably can use a ternary
 // Selection elements should display default values
+// Reduce opacity of irrelevant countries
+// Add hover effects for countries
