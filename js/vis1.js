ad2eefc22d7de7434decca61960302e77a90ae69
phrayse <80496902+phrayse@users.noreply.github.com>
2024-09-26 18:22:37 +1000

    refactor: better naming; filterData helper function; render functions drop redundant args

--- js/vis1.js
+++ js/vis1.js
@@ -1,38 +1,37 @@
-// Define constants
-// Dimensions
-const WIDTH = 960;
-const HEIGHT = 960;
-const LINE_MARGIN = {top: 20, right: 30, bottom: 40, left:50};
-const LINE_WIDTH = 500 - LINE_MARGIN.left - LINE_MARGIN.right;
-const LINE_HEIGHT = 300 - LINE_MARGIN.top - LINE_MARGIN.bottom;
+// Chart dimensions
+const CHOROPLETH_WIDTH = 960;
+const CHOROPLETH_HEIGHT = 960;
+const LINE_CHART_MARGIN = {top: 20, right: 30, bottom: 40, left:50};
+const LINE_CHART_WIDTH = 500 - LINE_CHART_MARGIN.left - LINE_CHART_MARGIN.right;
+const LINE_CHART_HEIGHT = 300 - LINE_CHART_MARGIN.top - LINE_CHART_MARGIN.bottom;
 
-// Colour and stroke
-const COLOUR_DEFAULT = "#666420";
-const COLOUR_HIGHLIGHT = "#420666";
-const COLOUR_EMPTY = "#808080"; // "empty" is not descriptive. "no value" is.
+// Colour and stroke width
+const COLOUR_BORDER_DEFAULT = "#666420";
+const COLOUR_BORDER_HIGHLIGHT = "#420666";
+const COLOUR_NO_VALUE = "#808080";
 const COLOUR_MALE = "#ffaaee";
 const COLOUR_FEMALE = "#eeffaa";
-const COLOUR_FRAME = "#CCCCCCCC"; // 8-digit hex, incl. opacity
-const WIDTH_DEFAULT = "0.5px";
-const WIDTH_HIGHLIGHT = "0.8px";
+const COLOUR_POPUP_FRAME = "#CCCCCCCC"; // 8-digit hex, incl. opacity
+const STROKE_WIDTH_DEFAULT = "0.5px";
+const STROKE_WIDTH_HIGHLIGHT = "0.8px";
+const STROKE_WIDTH_LINE = "1.5px";
 
 // Global variables
 let allData = [];
-let colourFlag = false;
+let altColour = false;
 let cause, year, country, sex, rangeYears;
 
 
-// Set up D3 elements
 // Set up SVG canvas
 let svg = d3.select("#choropleth")
     .append("svg")
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT);
+    .attr("width", CHOROPLETH_WIDTH)
+    .attr("height", CHOROPLETH_HEIGHT);
 
 // Set up map projection
 let projection = d3.geoMercator()
     .center([0, 0])
-    .translate([WIDTH / 2, HEIGHT / 2])
+    .translate([CHOROPLETH_WIDTH / 2, CHOROPLETH_HEIGHT / 2])
     .scale(150);
 
 let path = d3.geoPath(projection);
@@ -46,19 +45,27 @@ let popup = d3.select("body")
     .attr("id", "line-chart")
     .style("position", "absolute")
     .style("display", "none") // start hidden
-    .style("background-color", COLOUR_FRAME)
-    .style("border", `1px solid ${COLOUR_HIGHLIGHT}`)
+    .style("background-color", COLOUR_POPUP_FRAME)
+    .style("border", `1px solid ${COLOUR_BORDER_HIGHLIGHT}`)
     .style("padding", "10px")
     .style("border-radius", "5px")
     .style("pointer-events", "none"); // prevent frame blocking country change
 
 
-// TODO: func filterdata ({attributes}) or however for both choro and line filtering
+// Helper function for filtering data by cause, country, sex, or year
+function filterData(attributes = {}) {
+    let {cause, country, sex, year} = attributes;
+    return allData.filter(d =>
+        d.Cause === cause && // cause is only attr used in both charts, rest optional
+        (!country || d.Country === country) &&
+        (!sex || d.Sex === sex) &&
+        (!year || d.Year === year)
+    );
+}
 
-// Filter and merge CSV data and GeoJSON map /
-// Render map and apply fill per value
-function renderChoropleth(cause, year, sex) {
-    let filteredData = allData.filter(d => d.Cause === cause && d.Year === year && d.Sex === sex); // replace with helper function so can reuse for line chart
+// Filter and merge CSV data and GeoJSON map, then render choropleth with scaled fill colour
+function renderChoropleth() {
+    let filteredData = filterData({cause, year, sex});
     let dataMap = new Map();
     filteredData.forEach(d => dataMap.set(d.Country, d.Value));
 
@@ -71,15 +78,17 @@ function renderChoropleth(cause, year, sex) {
             feature.properties.value = dataMap.get(feature.properties.name) || null;
         });
 
-        // Create path elements
+        // Create path elements for countries
         svg.selectAll("path")
             .data(json.features)
             .join("path")
             .attr("class", "country")
             .attr("d", path)
-            .attr("fill", d => d.properties.value ? colour(d.properties.value) : COLOUR_EMPTY)
-            .attr("stroke", COLOUR_DEFAULT)
-            .attr("stroke-width", WIDTH_DEFAULT)
+            .attr("fill", d => d.properties.value
+                ? colour(d.properties.value)
+                : COLOUR_NO_VALUE)
+            .attr("stroke", COLOUR_BORDER_DEFAULT)
+            .attr("stroke-width", STROKE_WIDTH_DEFAULT)
             .on("mouseover", function(event, d) {
                 handleMouseOver.bind(this)(event, d); // bind 'this' before passing to handler
             })
@@ -91,141 +100,136 @@ function renderChoropleth(cause, year, sex) {
 
 // Add highlight and pop-up line chart on hovering a country
 function handleMouseOver(event, d) {
-    d3.select(this).style("stroke", COLOUR_HIGHLIGHT)
-        .style("stroke-width", WIDTH_HIGHLIGHT);
+    d3.select(this).style("stroke", COLOUR_BORDER_HIGHLIGHT)
+        .style("stroke-width", STROKE_WIDTH_HIGHLIGHT);
 
     let popupX = event.pageX + 15; // offset right
     let popupY = event.pageY + 15; // offset below
 
     // Prevent element clipping out of window
-    if (popupX + LINE_WIDTH > window.innerWidth) {
-        popupX = event.pageX - LINE_WIDTH - 15; // offset left if would clip right
+    if (popupX + LINE_CHART_WIDTH > window.innerWidth) {
+        popupX = event.pageX - LINE_CHART_WIDTH - 15; // offset left if would clip right
     }
 
-    if (popupY + LINE_HEIGHT > window.innerHeight) {
-        popupY = event.pageY - LINE_HEIGHT - 15; //offset above if would clip below
+    if (popupY + LINE_CHART_HEIGHT > window.innerHeight) {
+        popupY = event.pageY - LINE_CHART_HEIGHT - 15; //offset above if would clip below
     }
 
     // Set frame position
     popup.style("left", `${popupX}px`)
-            .style("top", `${popupY}px`)
-            .style("display", "block");
+        .style("top", `${popupY}px`)
+        .style("display", "block");
 
     country = d.properties.name;
-    lineChart(cause, country);
+    renderLineChart();
 }
 
 // Remove highlight and pop-up on mouseout
 function handleMouseOut() {
-    d3.select(this).style("stroke", COLOUR_DEFAULT)
-        .style("stroke-width", WIDTH_DEFAULT);
-    country = ""; // is this necessary anymore?
+    d3.select(this).style("stroke", COLOUR_BORDER_DEFAULT)
+        .style("stroke-width", STROKE_WIDTH_DEFAULT);
+    country = "";
     popup.style("display", "none");
     popup.select("svg").remove();
 }
 
 // Filters data by cause and country, then sex, then shows values for all years in a pop-up line chart
-function lineChart(cause, country) {
+function renderLineChart() {
     if (!country) return;
 
-    // Filter data to get correct country, cause
-    let filteredLineData = allData.filter(d => d.Cause === cause && d.Country === country); // swap w helper func
-    let maleData = filteredLineData.filter(d => d.Sex === "Male");
-    let femaleData = filteredLineData.filter(d => d.Sex === "Female");
+    // Filter data to get Male and Female values
+    let maleData = filterData({cause, country, sex: "Male"});
+    let femaleData = filterData({cause, country, sex: "Female"});
 
-    // Sort chronologically to get rid of line
+    // Sort data chronologically to get rid of the line between first and last values
     maleData.sort((a, b) => a.Year - b.Year);
     femaleData.sort((a, b) => a.Year - b.Year);
 
-    // debugging: check filtered data exists
+    // Check filtered data exists else exit
     if (maleData.length === 0 && femaleData.length === 0) {
         console.warn(`no data for country: ${country}; cause: ${cause}`);
         return;
     }
     
-    // clear existing line chart
+    // Clear existing line chart
     d3.select("#line-chart").select("svg").remove();
 
     // Create svg element
     let svgLine = d3.select("#line-chart")
-                    .append("svg")
-                    .attr("width", LINE_WIDTH + LINE_MARGIN.left + LINE_MARGIN.right)
-                    .attr("height", LINE_HEIGHT + LINE_MARGIN.top + LINE_MARGIN.bottom)
-                    .append("g")
-                    .attr("transform", `translate(${LINE_MARGIN.left}, ${LINE_MARGIN.top})`);
+        .append("svg")
+        .attr("width", LINE_CHART_WIDTH + LINE_CHART_MARGIN.left + LINE_CHART_MARGIN.right)
+        .attr("height", LINE_CHART_HEIGHT + LINE_CHART_MARGIN.top + LINE_CHART_MARGIN.bottom)
+        .append("g")
+        .attr("transform", `translate(${LINE_CHART_MARGIN.left}, ${LINE_CHART_MARGIN.top})`);
 
     // Create scales
     let xScaleLine = d3.scaleLinear()
-                        .domain(d3.extent(filteredLineData, d => d.Year))
-                        .range([0, LINE_WIDTH]);
+        .domain(d3.extent([...maleData, ...femaleData], d => d.Year)) // spread operator (...) concatenates the arrays
+        .range([0, LINE_CHART_WIDTH]);
 
     let yScaleLine = d3.scaleLinear()
-                        .domain([0, d3.max(filteredLineData, d => d.Value)])
-                        .range([LINE_HEIGHT, 0]);
+        .domain([0, d3.max([...maleData, ...femaleData], d => d.Value)])
+        .range([LINE_CHART_HEIGHT, 0]);
 
-    // debugging: country, years, max value
     console.log(`${country}; ${xScaleLine.domain()}; peak ${yScaleLine.domain()[1]}`);
 
     // Create axes
-    let xAxisLine = d3.axisBottom()
-                        .scale(xScaleLine);
-    
-    let yAxisLine = d3.axisLeft()
-                        .scale(yScaleLine);
+    let xAxisLine = d3.axisBottom(xScaleLine);
+    let yAxisLine = d3.axisLeft(yScaleLine);
 
     // Add axes
     svgLine.append("g")
-            .attr("transform", `translate(0, ${LINE_HEIGHT})`)
-            .call(xAxisLine);
+        .attr("transform", `translate(0, ${LINE_CHART_HEIGHT})`)
+        .call(xAxisLine);
             
     svgLine.append("g")
-            .call(yAxisLine);
+        .call(yAxisLine);
 
     // Create line
     let lineMarker = d3.line()
-                        .defined(d => !isNaN(d.Value)) // ignore missing data points
-                        .x(d => xScaleLine(d.Year))
-                        .y(d => yScaleLine(d.Value));
+        .defined(d => !isNaN(d.Value)) // ignore missing data points
+        .x(d => xScaleLine(d.Year))
+        .y(d => yScaleLine(d.Value));
 
     // Add lines
     svgLine.append("path")
-            .datum(maleData)
-            .attr("class", "line")
-            .attr("fill", "none")
-            .attr("stroke", COLOUR_MALE)
-            .attr("stroke-width", 1.5) // should be a const
-            .attr("d", lineMarker);
+        .datum(maleData)
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", COLOUR_MALE)
+        .attr("stroke-width", STROKE_WIDTH_LINE)
+        .attr("d", lineMarker);
 
     svgLine.append("path")
-            .datum(femaleData)
-            .attr("class", "line")
-            .attr("fill", "none")
-            .attr("stroke", COLOUR_FEMALE)
-            .attr("stroke-width", 1.5) // const
-            .attr("d", lineMarker);
+        .datum(femaleData)
+        .attr("class", "line")
+        .attr("fill", "none")
+        .attr("stroke", COLOUR_FEMALE)
+        .attr("stroke-width", STROKE_WIDTH_LINE)
+        .attr("d", lineMarker);
 
     // Add colour legend
     svgLine.append("text")
-            .attr("x", LINE_WIDTH)
-            .attr("y", LINE_MARGIN.top)
-            .attr("fill", COLOUR_MALE)
-            .attr("text-anchor", "end")
-            .text("Male");
+        .attr("x", LINE_CHART_WIDTH)
+        .attr("y", LINE_CHART_MARGIN.top)
+        .attr("fill", COLOUR_MALE)
+        .attr("text-anchor", "end")
+        .text("Male");
 
     svgLine.append("text")
-            .attr("x", LINE_WIDTH)
-            .attr("y", LINE_MARGIN.top + 20)
-            .attr("fill", COLOUR_FEMALE)
-            .attr("text-anchor", "end")
-            .text("Female");
+        .attr("x", LINE_CHART_WIDTH)
+        .attr("y", LINE_CHART_MARGIN.top + 20)
+        .attr("fill", COLOUR_FEMALE)
+        .attr("text-anchor", "end")
+        .text("Female");
 
     // Add Country label
     svgLine.append("text")
-            .attr("x", LINE_WIDTH)
-            .attr("y", LINE_HEIGHT + LINE_MARGIN.bottom)
-            .attr("fill", COLOUR_DEFAULT)
-            .attr("text-anchor", "end")
-            .text(country);
+        .attr("x", LINE_CHART_WIDTH)
+        .attr("y", LINE_CHART_HEIGHT + LINE_CHART_MARGIN.bottom)
+        .attr("fill", COLOUR_BORDER_DEFAULT)
+        .attr("text-anchor", "end")
+        .text(country);
 }
 
 // Takes the set of causes and creates a select menu with an option for each
@@ -238,24 +242,24 @@ function createCauses(uniqueCauses) {
 
     // Add select element
     let select = d3.select("#causes")
-                    .append("select")
-                    .attr("id", "cause-select");
+        .append("select")
+        .attr("id", "cause-select");
 
     // Bind options to select element
     select.selectAll("option")
-            .data(uniqueCauses)
-            .enter()
-            .append("option")
-            .attr("value", d => d)
-            .text(d => d);
+        .data(uniqueCauses)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
 
     cause = uniqueCauses[0]; // default to first value
     
     // Update choropleth and line chart on change
     select.on("change", function() {
         cause = this.value;
-        renderChoropleth(cause, year, sex);
-        lineChart(cause, country);
+        renderChoropleth();
+        renderLineChart();
     });
 }
 
@@ -285,26 +289,26 @@ function createSexes(sexes) {
     // Update choropleth on change
     select.on("change", function() {
         sex = this.value;
-        renderChoropleth(cause, year, sex);
+        renderChoropleth();
     });
 }
 
 // Takes array of [min, max] year and creates a slider.
 function createYears(rangeYears) {
-    let yearDiv = d3.select("#years");
-
     // Add label
-    let yearLabel = yearDiv.append("label")
+    let yearLabel = d3.select("#years")
+        .append("label")
         .attr("id", "year-label")
-        .text(`Year: ${rangeYears[0]}`); // default to min year
+        .text(`Year: ${rangeYears[0]}`);
 
     // Add slider element
-    let slider = yearDiv.append("input")
+    let slider = d3.select("#years")
+        .append("input")
         .attr("id", "year-slider")
         .attr("type", "range")
         .attr("min", rangeYears[0])
         .attr("max", rangeYears[1])
-        .attr("value", rangeYears[0]) // default to min year
+        .attr("value", rangeYears[0])
         .attr("step", 1);
 
     year = rangeYears[0]; // default to min year
@@ -313,7 +317,7 @@ function createYears(rangeYears) {
     slider.on("input", function() {
         year = +this.value; // slider returns str, convert to num
         yearLabel.text(`Year: ${year}`);
-        renderChoropleth(cause, year, sex);
+        renderChoropleth();
     });
 }
 
@@ -336,8 +340,6 @@ d3.select("body").on("keydown", function(event) {
         case "ArrowRight":
             changeYear(1);
             break;
-        default:
-            console.log(`${event.key}`);
     }
 });
 
@@ -353,20 +355,17 @@ function changeCause() {
     cause = causeSelect.options[nextIndex].value; // get value for new cause
 
     // Update choro and line
-    renderChoropleth(cause, year, sex);
-    lineChart(cause, country);
+    renderChoropleth();
+    renderLineChart();
 }
 
-// TODO: ternary
 // Toggles between primary and alternate colour palette
 function changeColour() {
-    colourFlag = !colourFlag;
-    if (colourFlag) {
-        colour = d3.scaleSequential(d3.interpolateGreens);
-    } else {
-        colour = d3.scaleSequential(d3.interpolatePurples);
-    }
-    renderChoropleth(cause, year, sex);
+    altColour = !altColour;
+    colour = altColour
+        ? d3.scaleSequential(d3.interpolateGreens)
+        : d3.scaleSequential(d3.interpolatePurples);
+    renderChoropleth();
 }
 
 function changeSex() {
@@ -378,8 +377,8 @@ function changeSex() {
     sexSelect.selectedIndex = nextIndex;
     sex = sexSelect.options[nextIndex].value;
 
-    // Update choro
-    renderChoropleth(cause, year, sex);
+    // Update choropleth
+    renderChoropleth();
 }
 
 // Changes year if new year is not out of range, updates slider, label, and choropleth
@@ -392,7 +391,7 @@ function changeYear(direction) {
 
     d3.select("#year-slider").property("value", `${year}`);
     d3.select("#year-label").text(`Year: ${year}`);
-    renderChoropleth(cause, year, sex);
+    renderChoropleth();
 }
 
 
@@ -420,5 +419,5 @@ d3.csv("../data/causes/all-top-level-causes.csv").then(function(data) {
     createYears(rangeYears);
 
     // Render initial view with default options
-    renderChoropleth(cause, year, sex);
+    renderChoropleth();
 }).catch(error => console.error("Error fetching CSV data: ", error));
